80b11784f57c5588d9c36aec6303c3932cfd0116
Juan Pablo Sarmiento <pablowritescode@gmail.com>
2024-05-01 02:59:48 -0400

    Updated constants

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,3 +1,3 @@
 export const API_BASE_URL: string = import.meta.env.VITE_HERA_BACKEND_API;
 
-export const DASHBOARDS = ['main-dashboard.json', 'new-dashboard.json'];
+export const DASHBOARDS = ['main-dashboard.json'];
